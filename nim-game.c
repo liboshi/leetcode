827772e9deb16bff2c205b49cf14687b5a94048b
Boush Li <li.boush@gmail.com>
2015-10-20 22:00:48 +0800

    o Add answer for Nim Game.

---
+++ nim-game.c
@@ -0,0 +1,22 @@
+/*
+ * Nim Game
+ */
+#include <stdio.h>
+
+typedef int bool;
+enum {
+        false,
+        true
+};
+
+bool
+canWinNim(int n)
+{
+        return !(n % 4 == 0);
+}
+
+bool
+main()
+{
+        return canWinNim(7);
+}
