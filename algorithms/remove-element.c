d21f496e72f3e13241b3bd4cce2f686e99a96e78
Boshi Li <li.boush@gmail.com>
2017-08-27 15:07:38 +0800

    o Add answer for remove element.
    Signed-off-by: Boshi Li <li.boush@gmail.com>

---
+++ algorithms/remove-element.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int removeElement(int* nums, int numsSize, int val) {
+        int count = 0;
+        for (int i = 0; i < numsSize; i++) {
+                if (nums[i] == val)
+                        count++;
+                else
+                        nums[i - count] = nums[i];
+        }
+        return numsSize - count;
+}
+
+int main() {
+        return 0;
+}
