eca4eda2deefe88d717e7336834740565d5448cd
Boshi Li <li.boush@gmail.com>
2017-09-04 21:32:13 +0800

    o Add definition for TreeNode.
    Signed-off-by: Boshi Li <li.boush@gmail.com>

--- algorithms/invert-binary-tree.c
+++ algorithms/invert-binary-tree.c
@@ -2,10 +2,23 @@
  * Invert binary tree
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+
+struct TreeNode {
+        int val;
+        struct TreeNode* right;
+        struct TreeNode* left;
+};
+
 struct TreeNode* invertTree(struct TreeNode* root) {
         if (root == NULL) return root;
         struct TreeNode* node = invertTree(root->right);
         root->right = invertTree(root->left);
         root->left = node;
         return root;
+}
+
+int main() {
+        return 0;
 }
